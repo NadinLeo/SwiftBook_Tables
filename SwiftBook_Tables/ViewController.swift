217f027f04c75f3c2001c234445a12f023df137b
Nadzeya <Nadezhda.Leonovich@gmail.com>
2019-10-03 22:11:53 +0300

    Initial Commit

---
+++ SwiftBook_Tables/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBook_Tables
+//
+//  Created by Nadzeya Leanovich on 10/3/19.
+//  Copyright © 2019 Nadzeya Leanovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
