57cccfc2021421f0d616b8e3f95ca6ff8b888f9e
Nadzeya <Nadezhda.Leonovich@gmail.com>
2019-10-04 20:21:50 +0300

    My places: init commit

---
+++ SwiftBook_Tables/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainViewController.swift
+//  SwiftBook_Tables
+//
+//  Created by Nadzeya Leanovich on 10/3/19.
+//  Copyright © 2019 Nadzeya Leanovich. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let lovelyPlaces = ["Paul", "Bistro Delux", "Animal Farm", "LIDO", "Victory Park"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+      return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return lovelyPlaces.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = lovelyPlaces[indexPath.row]
+
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
